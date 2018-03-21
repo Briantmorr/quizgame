87bb1538fa53774384c305a773180d0c6a96dcdc
briantmorr <briantmorr@gmail.com>
2018-03-21 10:49:24 -0700

    temp questions

--- model.js
+++ model.js
@@ -1,1 +1,52 @@
 //start with an array of 8 questions that are true/false or multiple choice
+
+var questionArray = [
+    {
+        id: 1, 
+        question: "cattle have four stomachs to digest grass.",
+        type:'true/false',
+        answer:'true'
+    },
+    {
+        id: 2, 
+        question: "an ostrich's eye is bigger than its brain",
+        type:'true/false',
+        answer:'true'
+    },
+    {
+        id: 3, 
+        question: "an ant can live to be 29 years old",
+        type:'true/false',
+        answer:'true'
+    },
+    {
+        id: 4, 
+        question: "a mole can dig a tunnel 500 feet long in just one night",
+        type:'true/false',
+        answer:'false'
+    },
+    {
+        id: 5, 
+        question: "cattle have four stomachs to digest grass.",
+        type:'true/false',
+        answer:'true'
+    },
+    {
+        id: 6, 
+        question: "cattle have four stomachs to digest grass.",
+        type:'true/false',
+        answer:'true'
+    },
+    {
+        id: 7, 
+        question: "cattle have four stomachs to digest grass.",
+        type:'true/false',
+        answer:'true'
+    },
+    {
+        id: 8, 
+        question: "cattle have four stomachs to digest grass.",
+        type:'true/false',
+        answer:'true'
+    },
+];
