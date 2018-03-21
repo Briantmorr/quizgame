00617294831aff5e3c0496c6d58617905ab79a34
briantmorr <briantmorr@gmail.com>
2018-03-21 11:55:31 -0700

    view ready for testing

--- view.js
+++ view.js
@@ -1,47 +1,78 @@
 // render questions into view.html
 
-class View{
+class View {
 
-    constructor(gameController){
+    constructor(gameController) {
         this.gameController = gameController;
     }
-    
+
     renderAllQuestions() {
         for (var i = 0; i < questionArray.length; i++) {
-            this.renderEachQuestion(questionArray[i]);
+            this.renderEachQuestion(questionArray[i], i);
         }
     }
-    renderEachQuestion(question){
-
+    renderEachQuestion(question, i) {
+        var qDiv = $('<div>', {
+            id: i
+        });
+        var qQuestion = $('<p>', {
+            text: `${i}) ${question.question}`
+        });
+        if (question.type === 'multiple') {
+            var qChoices = $('<div>');
+            for (var i = 0; i < question.choices.length; i++) {
+                var qChoice = $('<input>', {
+                    type: 'radio',
+                    value: question.choices[i]
+                });
+                qChoices.append(qChoice);
+            }
+        }
+        else {
+            var qChoices = $('<div>');
+            var qChoice = $('<input>', {
+                type: 'radio',
+                value: 'true'
+            });
+            var qChoice1 = $('<input>', {
+                type: 'radio',
+                value: 'false'
+            });
+            qChoices.append(qChoice, qChoice1);
+        }
+        qDiv.append(qQuestion, qChoices);
+        console.log('qDiv is: ', qDiv);
+        $('main').append(qDiv);
     }
 
-    createAnswersObjectFromDOMElements(){
+
+    createAnswersObjectFromDOMElements() {
 
         var userAnswers = [];
         var questions = this.gameController.getQuestions();
 
-        for(let x = 0; x < questions.length; x++){
+        for (let x = 0; x < questions.length; x++) {
             var radios = document.getElementsById(questions[x].id);
 
-            for (let i = 0; i < radios.length; i++){
-                if (radios[i].checked){       
+            for (let i = 0; i < radios.length; i++) {
+                if (radios[i].checked) {
                     userAnswers.push({
                         id: questions[x].id,
                         answer: radios[i].value
-                    });      
+                    });
                 }
             }
         }
         return userAnswers;
     }
 
-    checkUserAnswers(){
+    checkUserAnswers() {
         var userAnswers = this.createAnswersObjectFromDOMElements();
         var finalResult = this.gameController.checkAnswers(userAnswers);
-        if(finalResult){
+        if (finalResult) {
             var passElement = document.getElementsById('passMessage');
             passElement.classList.remove("hidden");
-        }else{
+        } else {
             var taButton = document.getElementsById('tryAgainButton');
             taButton.classList.remove("hidden");
         }
