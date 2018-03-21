a66ec28e2b81d638521c9d50fc4764d0960e5dee
briantmorr <briantmorr@gmail.com>
2018-03-21 11:13:09 -0700

    start of view

--- view.js
+++ view.js
@@ -1,2 +1,22 @@
 // render questions into view.html
+var gameView = null;
+var gameController = null;
+$(document).ready(initializeApp){
+    gameController = new Controller();
+    gameView = new View();
+    
+}
+class View{
+    constructor(){
+        gameController.setQuestions(questionArray);
+    }
+    renderAllQuestions() {
+        for (var i = 0; i < questionArray.length; i++) {
+            this.renderEachQuestion(questionArray[i]);
+        }
+    }
+    renderEachQuestion(question){
 
+    }
+
+}
